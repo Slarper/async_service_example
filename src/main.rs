eede4d802ec21f9310e883eb2eb2a357ea2090fe
Slarper <you@example.com>
2024-12-04 00:32:56 +0800

    a better example with local, dynamic structured concurrency;Refactor main.rs to use async_scope! macro for managing async tasks

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,36 @@
-use futures_concurrency::prelude::*;
 
 
+use moro_local::async_scope;
 
 // #[tokio::main(flavor = "current_thread")]
 pub fn main() {
-let rt = tokio::runtime::Builder::new_current_thread()
+    let rt = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
         .expect("build runtime");
 
-    rt.block_on(async move {
-
+    rt.block_on(async {
         let mut container = vec![1, 2, 3];
         let mut num = 0;
-
-        let a = async {
-            println!("hello from the first future");
-            dbg!(&container);
-        };
-
-        let b = async {
-            println!("hello from the second future");
-            num += container[0] + container[2];
-        };
-
         println!("hello from the main future");
-        let _ = (a, b).join().await;
+        async_scope!(|scope| {
+            let a = async {
+                println!("hello from the first future");
+                dbg!(&container);
+            };
+
+            let b = async {
+                println!("hello from the second future");
+                num += container[0] + container[2];
+            };
+            scope.spawn(a);
+            scope.spawn(b);
+        }).await;
+
+        
         container.push(4);
         assert_eq!(num, container.len());
-    })
+        println!("container: {:?}", container);
+        println!("num: {:?}", num);
+    });
 }
